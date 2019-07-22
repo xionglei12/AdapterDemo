3590fa049b593c5cf4ee5e8c841e8ec37fb29bbc
000778 <xionglei@fcbox.com>
2019-07-22 18:03:25 +0800

    <接入其中一种适配>

--- app/src/main/java/com/fcbox/adapterdemo/MainActivity.java
+++ app/src/main/java/com/fcbox/adapterdemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.fcbox.adapterdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class MainActivity extends AutoLayoutActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ImageView iv = findViewById(R.id.iv);
+		ViewGroup.LayoutParams layoutParams = iv.getLayoutParams();
+		int width = layoutParams.width;
+		int height = layoutParams.height;
+		Log.d("xl","width:"+width+"height:"+height);
+		// todo 发现并没有很好的适配
 	}
 }
